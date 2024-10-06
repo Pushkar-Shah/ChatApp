a7c1d0f107029a24f3f6b1e7c97644f7e67bd0ab
Pushkar Shah <162821367+Pushkar-Shah@users.noreply.github.com>
2024-10-06 18:37:57 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-    // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  ,
   server: {
-    host: '0.0.0.0', // Allow external access
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173 // Use PORT from the environment
+    host: "0.0.0.0", // Allow external access
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000, // Use PORT from environment or default to 3000
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,  // Increase the chunk size limit
-  },
-})
-
-
+});
